1c704620da21e9a5607e8e4fb3fb7e682b7d2f0e
Ian Leckey <ian.leckey@gmail.com>
2023-02-13 14:23:43 +0000

    preflight cors

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 import { Router } from 'itty-router';
+import { createCors } from 'itty-cors'
+
 import CheckAddress from './handlers/checkaddress';
+
+// create CORS handlers
+const { preflight, corsify } = createCors()
 
 const router = Router();
 
 router
+	.all('*', preflight)
     .get('/api/checkaddress/:address', CheckAddress)
     .get('*', () => new Response('Not found', { status: 404 }));
 
 export default {
-	fetch: (request, env, context) => router.handle(request, env, context).then(response => {
-		return response
-	}).catch(err => {
-		// and do something with the errors here, like logging, error status, etc
-		console.log(err)
-	})
+	fetch: (request, env, context) => router
+		.handle(request, env, context)
+		.catch(err => {
+			console.log(err)
+		})
+		.then(corsify)
 }
