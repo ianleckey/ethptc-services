910141b778e501cf675e2cac1e9729b040752df3
Ian Leckey <ian.leckey@gmail.com>
2023-02-14 18:47:15 +0000

    lookup existing profiles

--- src/handlers/checkaddress.ts
+++ src/handlers/checkaddress.ts
@@ -1,4 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+
 const CheckAddress = async (request, env, context) => {
+
+    const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY)
+
     // This will be used soon to retrieve a post
     const address = request.params.address;
     const cache = caches.default
@@ -7,20 +12,41 @@ const CheckAddress = async (request, env, context) => {
         return response
     }
 
-    const fpResponse = await (
-        await fetch('https://faucetpay.io/api/v1/checkaddress', {
-            method: 'post',
-            body: 'api_key='+ env.FPKEY +'&address='+ address,
-            headers: { 'Content-type': 'application/x-www-form-urlencoded' }
-        })
-    ).json()
 
-    response = new Response(JSON.stringify(fpResponse), {
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Cache-Control': 'max-age: 3600',
-        },
-    })
+    let { data, error, status, count } = await supabase
+        .from('profiles')
+        .select(`*`, { count: 'exact', head: true })
+        .eq('fp_address', address)
+
+    if(count > 0 || error) {
+
+        response = new Response(JSON.stringify({ status: 403 }), {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Cache-Control': 'max-age: 3600',
+            },
+        })
+
+    }
+
+    if(count === 0) {
+
+        const fpResponse = await (
+            await fetch('https://faucetpay.io/api/v1/checkaddress', {
+                method: 'post',
+                body: 'api_key='+ env.FPKEY +'&address='+ address,
+                headers: { 'Content-type': 'application/x-www-form-urlencoded' }
+            })
+        ).json()
+
+        response = new Response(JSON.stringify(fpResponse), {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Cache-Control': 'max-age: 3600',
+            },
+        })
+
+    }
 
     context.waitUntil(cache.put(request, response.clone()))
 
